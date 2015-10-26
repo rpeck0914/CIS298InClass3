a7b49158ad221b0a9565e6eccd1779e2c33e3e49
Robert Peck <rpeck0914@mail.kvcc.edu>
2015-10-26 14:53:57 -0400

    Add a textchanged listener to the edit test

--- app/src/main/java/edu/kvcc/cis298/inclass3/inclass3/CrimeFragment.java
+++ app/src/main/java/edu/kvcc/cis298/inclass3/inclass3/CrimeFragment.java
@@ -5,15 +5,20 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 
 public class CrimeFragment extends Fragment {
 
     //Declare A Class Level Variable For A Crime
     private Crime mCrime;
+
+    private EditText mTitleField;
 
     //This Method Does Not Do The Inflating Of The View
     //Like the onCreate For An Activity Does
@@ -29,6 +34,25 @@ public class CrimeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_crime, container, false);
+
+        mTitleField = (EditText) v.findViewById(R.id.crime_title);
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                //This Space Intentionally Left Blank
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                mCrime.setmTitle(charSequence.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                //This One Too
+            }
+        });
+
         return v;
     }
 }
